1980edf187282ffe8dbd0f2dee4ae6fc5a7cf1eb
kevinorta <kevino6532@hstat.org>
2016-11-06 10:38:52 +0000

    broken line count

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def line(katz_deli)
+     
+    if katz_deli.empty? 
+        puts "The line is currently empty."
+    els
+       katz_deli.each.with_index(1){ |name, count|
+           puts "The line is currently: #{count+1}. #{name}"
+       }
+    end
+end
