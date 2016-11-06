eecf560467716933f6bde0d91d1afe5b615317c2
kevinorta <kevino6532@hstat.org>
2016-11-06 10:53:22 +0000

    done

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,24 @@ def line(katz_deli)
      
     if katz_deli.empty? 
         puts "The line is currently empty."
-    els
-       katz_deli.each.with_index(1){ |name, count|
-           puts "The line is currently: #{count+1}. #{name}"
+    else
+        deliLine="The line is currently:"
+       katz_deli.each.with_index{ |name, count|
+           deliLine << " #{count+1}. #{name}"
        }
+       puts deliLine
+    end
+end
+
+def take_a_number(katz_deli, name)
+    katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+    if katz_deli.empty?
+        puts "There is nobody waiting to be served!"
+    else
+       puts "Currently serving #{katz_deli.shift}."
     end
 end
